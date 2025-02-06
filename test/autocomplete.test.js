927a39647940452085e0d924a465b178f47b8d76
L C <LF9904@gmail.com>
2025-02-05 21:35:12 -0500

    implemented WaitFor

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,19 +1,49 @@
-it("Shows an autocomplete", () => {
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) =>{
+    const interval = setInterval(() => {
+      if(document.querySelector(selector)){
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+   const timeout = setTimeout(()=>{
+      clearInterval(interval);
+      reject();
+    },2000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   creatAutocomplete({
     root: document.querySelector("#target"),
-    fetchApi(){
-        return [
-            {Title: 'Avenegers'},
-            {Title: 'Not Avengers'},
-            {Title: 'Some other movie'}
-        ];
+    fetchApi() {
+      return [
+        {Title: "Avenegers"},
+        {Title: "Not Avengers"},
+        {Title: "Some other movie"},
+      ];
     },
     renderOption(movie) {
-        return movie.Title;
-    }
+      return movie.Title;
+    },
   });
+});
+
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching dropsdown opens up", async() => {
+  const input = document.querySelector("input");
+  input.value = "Avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor('.dropdown-item');
 
   const dropdown = document.querySelector(".dropdown");
-  
-  expect(dropdown.className).not.to.include("is-active");
+  expect(dropdown.className).to.include("is-active");
 });
